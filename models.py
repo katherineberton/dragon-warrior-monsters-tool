b90163b5da302c6050af6c8b22db463f71383d37
Kat Berton <katherine.berton@gmail.com>
2024-07-02 17:02:32 -0700

    initial commit

---
+++ models.py
@@ -0,0 +1,19 @@
+class Monster:
+    """Monsters owned by users."""
+    pass
+
+class MonsterCategory:
+    """Monster species."""
+    pass
+
+class Gate:
+    """A travelers gate through which monsters can be collected."""
+
+class MonsterGateMembership:
+    """Gates and levels in which a particular monster species can be caught in the wild."""
+    pass
+
+class BreedingPlan:
+    """A plan for users to assign their monsters to."""
+    def get_missing_steps(self):
+        pass
